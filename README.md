4088ae600b1d14761254611fba2c00942bd43cf0
王元林 <wangyuanlin@migu.cn>
2020-08-12 18:17:00 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SharedWorker
-SharedWorker demo
+用SharedWorker实现订阅发布者
+
+[mdn官方demo见此处](https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Workers_API/Using_web_workers)
+[网友改造版本见此处](https://github.com/xiangwenhu/page-communication)
+
+以上两种方案的最大问题就是没办法**指定某些页面之间进行通讯**，
+比如我在sharedWorker1.html页面中想指定发送信息到sharedWorker3.html, 就需要在以上demo基础上存储port并添加标识。最终实现真正意义上的指哪打哪。
+
+核心功能： 实现同源页面之间的指定通讯
+
+tip: 需要调试SharedWorker请使用 chrome://inspect/#workers 打开调试器
---
+++ shared_worker.js
@@ -0,0 +1,48 @@
+/**
+ * SharedWorker实现订阅发布者
+ */
+const portInfoList = []
+
+self.onconnect = function(e) {
+    const selfPort = e.ports[0]
+    ensurePorts(selfPort)
+
+    console.log(e, 'onconnect e')
+
+    selfPort.onmessage = function(e) {
+        // 初始化
+        if (e.data && e.data.type === 'init') {
+            const portInfo = portInfoList.find(portInfo => portInfo.port === selfPort)
+
+            // 外部参数(通常用于确定是属于哪个页面的port)
+            if (portInfo) portInfo.options = e.data.options
+        } else {
+            dispatch(selfPort, e.data)
+        }
+    }
+}
+
+// 订阅
+function ensurePorts(port) {
+    if (portInfoList.indexOf(port) < 0) {
+        portInfoList.push({ port })
+    }
+}
+
+// 发布
+function dispatch(selfPort, data) {
+    // 发布到指定页面
+    if (data && data.url) {
+        const portInfo = portInfoList.find(portInfo => portInfo.options && portInfo.options.url === data.url)
+        if (portInfo) portInfo.port.postMessage(data)
+    } else {
+        // 发布到除自身外的其他所有页面
+        portInfoList
+            .filter(portInfo => selfPort !== portInfo.port)
+            .forEach(portInfo => portInfo.port.postMessage(data))
+    }
+}
+
+setTimeout(() => {
+    console.log(portInfoList, 'portInfoList')
+}, 6000)
